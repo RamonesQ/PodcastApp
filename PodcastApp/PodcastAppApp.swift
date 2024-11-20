a297cf60df7c6e2f4cd7102ae6b72a188909748c
Ramon Queiroz <ramonqueirozs@gmail.com>
2024-11-20 17:08:50 -0300

    Initial commit

---
+++ PodcastApp/PodcastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastAppApp.swift
+//  PodcastApp
+//
+//  Created by Ramon Queiroz dos Santos on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PodcastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
