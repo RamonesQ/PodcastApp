91be8970809c2d490798686752a24916e80f3e7c
Ramon Santos <ramonqueirozs@gmail.com>
2024-12-23 12:53:45 -0300

    change cache to filemanager

--- PodcastApp/Services/PodcastStorage.swift
+++ PodcastApp/Services/PodcastStorage.swift
@@ -15,48 +15,71 @@ protocol PodcastStorageProtocol {
 }
 
 class PodcastStorage: PodcastStorageProtocol {
-    private let userDefaults = UserDefaults.standard
-    private let cachedPodcastsKey = "cachedPodcasts"
-    
-    private var cachedPodcasts: [String: Data] {
-        get { userDefaults.dictionary(forKey: cachedPodcastsKey) as? [String: Data] ?? [:] }
-        set { userDefaults.set(newValue, forKey: cachedPodcastsKey) }
+    private let fileManager = FileManager.default
+    private let cacheDirectory: URL
+    private let metadataFile: URL
+
+    init() {
+        let cachesDirectory = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        cacheDirectory = cachesDirectory.appendingPathComponent("PodcastCache")
+        metadataFile = cacheDirectory.appendingPathComponent("metadata.plist")
+        try? fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
     }
-    
+
+    private func getUniqueFilename(for url: String) -> String {
+        let urlData = Data(url.utf8)
+        return urlData.base64EncodedString().replacingOccurrences(of: "/", with: "_")
+    }
+
+    private func saveMetadata(_ metadata: [String: String]) {
+        (metadata as NSDictionary).write(to: metadataFile, atomically: true)
+    }
+
+    private func loadMetadata() -> [String: String] {
+        (NSDictionary(contentsOf: metadataFile) as? [String: String]) ?? [:]
+    }
+
     func savePodcast(_ podcast: Podcast, forURL url: String) throws {
-        do {
-            let encodedData = try JSONEncoder().encode(podcast)
-            var updatedCache = cachedPodcasts
-            updatedCache[url] = encodedData
-            cachedPodcasts = updatedCache
-        } catch {
-            throw PodcastAppError.encodingError
-        }
+        let encodedData = try JSONEncoder().encode(podcast)
+        let filename = getUniqueFilename(for: url)
+        let fileURL = cacheDirectory.appendingPathComponent(filename)
+        try encodedData.write(to: fileURL)
+
+        var metadata = loadMetadata()
+        metadata[url] = filename
+        saveMetadata(metadata)
     }
-    
+
     func loadPodcast(forURL url: String) throws -> Podcast {
-        guard let data = cachedPodcasts[url] else {
+        let metadata = loadMetadata()
+        guard let filename = metadata[url] else {
             throw PodcastAppError.dataNotFound
         }
-        
-        do {
-            return try JSONDecoder().decode(Podcast.self, from: data)
-        } catch {
-            throw PodcastAppError.decodingError
-        }
+        let fileURL = cacheDirectory.appendingPathComponent(filename)
+        let data = try Data(contentsOf: fileURL)
+        return try JSONDecoder().decode(Podcast.self, from: data)
     }
-    
+
     func getAllCachedPodcasts() -> [String: Podcast] {
         var result: [String: Podcast] = [:]
-        for (url, data) in cachedPodcasts {
-            if let podcast = try? JSONDecoder().decode(Podcast.self, from: data) {
+        let metadata = loadMetadata()
+
+        for (url, filename) in metadata {
+            let fileURL = cacheDirectory.appendingPathComponent(filename)
+            do {
+                let data = try Data(contentsOf: fileURL)
+                let podcast = try JSONDecoder().decode(Podcast.self, from: data)
                 result[url] = podcast
+            } catch {
+                print("Error loading podcast from file: \(error)")
             }
         }
+
         return result
     }
-    
+
     func clearCache() {
-        cachedPodcasts = [:]
+        try? fileManager.removeItem(at: cacheDirectory)
+        try? fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
     }
 }
